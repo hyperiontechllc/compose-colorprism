d0408769d8a772d276f6c4119c9b45b5b6ee5969
Valdrin <valimaloku23@gmail.com>
2025-10-28 15:57:07 +0100

    [MOBDEV-3] RC
    * Code refactor

--- colorprism/build.gradle.kts
+++ colorprism/build.gradle.kts
@@ -59,11 +59,5 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.foundation)
     testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
     testImplementation(kotlin("test"))
 }
